e46c4f11d6d06f7ddaca7baf52a4583393f93e91
yusuftnc <yusuf.tnc@gmail.com>
2025-06-11 14:13:41 +0300

    add x-api-key for development

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,2 +1,2 @@
-export const X_API_KEY = process.env.X_API_KEY;
+export const X_API_KEY = process.env.X_API_KEY || "4b679ab8-8d82-47be-9145-c45b9105fae9";
 export const OLLAMA_API_URL = process.env.OLLAMA_API_URL || 'http://localhost:11434';
